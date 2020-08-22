fdfe8274a2aca4b1f90958a0af5763ed99b26f94
spashii <sameer.pashikanti@gmail.com>
2020-08-22 06:04:04 +0530

    Updates

---
+++ precache-manifest.b55f793d54555d01dae9621e38a28fe1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e13245f646060f72745e5c050af45ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1246f4fb38749962c323",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "c26b66de387e6d4df456",
+    "url": "/static/css/main.67b3868e.chunk.css"
+  },
+  {
+    "revision": "1246f4fb38749962c323",
+    "url": "/static/js/2.b1236fe5.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.b1236fe5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c26b66de387e6d4df456",
+    "url": "/static/js/main.26495eda.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
